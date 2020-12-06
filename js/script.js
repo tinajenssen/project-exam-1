f2c0b8e26f1c539cf6625191a966fef3a5344663
tinajenssen <63742433+tinajenssen@users.noreply.github.com>
2020-12-06 18:20:57 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+// Navigation - hamburger-menu 
+const hamburger = document.getElementById("hamburger");
+const navUL = document.getElementById("nav-ul");
 
+hamburger.addEventListener("click", () => {
+    navUL.classList.toggle("show");
+});
+
+
+
+// Timeline 
+const timeline = document.getElementById("timeline-btn");
+const history = document.getElementById("milestones");
+
+timeline.addEventListener("click", () => {
+    history.classList.toggle("show");
+});
